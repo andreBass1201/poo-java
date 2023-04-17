3ecd691dee905c2294d3ea683c1b0fb54f12b825
andreBass1201 <andre300.av@gmail.com>
2023-04-17 08:33:56 -0500

    #

---
+++ MyMedicalProyect/src/Main.java
@@ -0,0 +1,15 @@
+import model.Doctor;
+
+import java.util.Date;
+
+import static ui.UIMenu.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        //showMenu();
+        //Patient paciente1 = new Patient("Juan", "juan@gmail.com");
+
+
+    }
+}
---
+++ MyMedicalProyect/src/model/Nurse.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Nurse {
+    private String speciality;
+
+    public Nurse(String name, String email) {
+        super(name, email);
+    }
+
+    @Override
+    public void m() {
+
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+}
